2b5d505ab46bb6bb5a13d5e60521724dfed5b127
ISAAC FERNANDES <isaacnfernandes@gmail.com>
2021-02-04 22:15:40 -0300

    Mensagem de teste direto do git

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		System.out.println("Testando o Github");
-
+		System.out.println("Testando o Github novamente.");
+		System.out.println("Testando o Github e elimininando espaços");
 	}
 
 }
